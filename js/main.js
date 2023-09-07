fb4dbf159434932cdb5adbf763ee07f16cda0b6b
aburayhan12 <136449209+aburayhan12@users.noreply.github.com>
2023-09-07 21:46:51 +0600

    add another bulb with mouseover event

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 var effect = document.getElementById('bulbOn');
-effect.addEventListener('click', function(){
+
+effect.addEventListener('click', function () {
     var image = document.getElementById('bulb');
     image.setAttribute('src', 'image/pic_bulbon.gif');
 });
 
 var effect = document.getElementById('bulbOff');
-effect.addEventListener('click', function(){
+
+effect.addEventListener('click', function () {
     var image = document.getElementById('bulb');
     image.setAttribute('src', 'image/pic_bulboff.gif');
 });
 
 
 var effect = document.getElementById('bulbOn2');
-effect.addEventListener('mouseover', function(){
+
+effect.addEventListener('mouseover', function () {
     var image = document.getElementById('bulb2');
     image.setAttribute('src', 'image/pic_bulbon.gif');
 });
 
 var effect = document.getElementById('bulbOff2');
-effect.addEventListener('mouseover', function(){
+
+effect.addEventListener('mouseover', function () {
     var image = document.getElementById('bulb2');
     image.setAttribute('src', 'image/pic_bulboff.gif');
 });
