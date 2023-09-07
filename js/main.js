1190a61844398b175300159fc75cce0861fedaca
aburayhan12 <136449209+aburayhan12@users.noreply.github.com>
2023-09-07 21:42:15 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 var effect = document.getElementById('bulbOn');
-
 effect.addEventListener('click', function(){
     var image = document.getElementById('bulb');
     image.setAttribute('src', 'image/pic_bulbon.gif');
 });
 
 var effect = document.getElementById('bulbOff');
-
 effect.addEventListener('click', function(){
     var image = document.getElementById('bulb');
+    image.setAttribute('src', 'image/pic_bulboff.gif');
+});
+
+
+var effect = document.getElementById('bulbOn2');
+effect.addEventListener('mouseover', function(){
+    var image = document.getElementById('bulb2');
+    image.setAttribute('src', 'image/pic_bulbon.gif');
+});
+
+var effect = document.getElementById('bulbOff2');
+effect.addEventListener('mouseover', function(){
+    var image = document.getElementById('bulb2');
     image.setAttribute('src', 'image/pic_bulboff.gif');
 });
